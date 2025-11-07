7ade08bbed5e95e2e3d2ff1e39e38839700c31f9
ropriana <manurungpia1@gmail.com>
2025-11-07 17:38:11 +0700

    pia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
   <!-- [Head] start -->
 
   <head>
-    <title>Home | Datta Able Dashboard Template</title>
+    <title>Home | Datta Able Dashboard Template pia</title>
     <!-- [Meta] -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" />
