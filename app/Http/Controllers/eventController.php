5282d108f268b3b2a65981bed96b92f97afc39a9
Agus Raj Pranata <agusrajprnataa@gmail.com>
2025-11-17 09:28:34 +0700

    baru seed

--- app/Http/Controllers/eventController.php
+++ app/Http/Controllers/eventController.php
@@ -1,20 +1,49 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Event;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::latest()->get();
-        return view('admin.event.index', compact('events'));
+        return response()->json(
+            Event::with(['penyelenggara', 'admin'])->get()
+        );
     }
 
-    public function create()
+    public function store(Request $request)
     {
-        return view('admin.event.create');
+        $request->validate([
+            'nama_event' => 'required',
+            'tanggal_mulai' => 'required',
+            'tanggal_selesai' => 'required',
+            'lokasi' => 'required',
+            'deskripsi' => 'required',
+            'kategori_event' => 'required',
+            'status' => 'required',
+            'penyelenggara_id' => 'required',
+            'id_admin' => 'required',
+        ]);
+
+        $data = Event::create($request->all());
+
+        return response()->json(['success' => true, 'data' => $data]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = Event::findOrFail($id);
+        $data->update($request->all());
+
+        return response()->json(['success' => true]);
+    }
+
+    public function destroy($id)
+    {
+        Event::findOrFail($id)->delete();
+        return response()->json(['success' => true]);
     }
 }
